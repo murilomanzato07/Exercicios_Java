4a1ff878f15b8453f33e05a77554fb8c5e383202
unknown <dinamite1010@gmail.com>
2025-09-02 11:13:52 -0300

    atualizacao feita

--- DecimoSegundo_Exercicio.java
+++ DecimoSegundo_Exercicio.java
@@ -1,5 +1,46 @@
+import java.util.Scanner;
 public class DecimoSegundo_Exercicio {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        int vetor1[], vetor2[], vetorFinal[];
+        vetor1 = new int[10];
+        vetor2 = new int[10];
+        vetorFinal = new int[10];
+        int cont=0;
+
+    for(int i = 0; i<10; i++){
+        System.out.printf("Digite %d - numero: ",i+1);
+        vetor1[i] = sc.nextInt();
+    }
+    for(int i = 0; i<10; i++){
+        System.out.printf("Digite %d - numero: ",i+1);
+        vetor2[i] = sc.nextInt();
+    }
+    for(int j = 0; j < 10; j++){
+
+    for (int i = 0; i < 10; i++) {
+        if (vetor1[i] == vetor2[j]) {
+            vetorFinal[j] = vetor1[i];
+            cont++;
+            }
+        }
+    }
+    System.out.printf("Valores repetidos = {");
+        if (cont > 0) {
+            for (int j = 0 ; j < 10; j++) {
+                if (vetorFinal[j] != 0 && j<9) {
+                    System.out.printf("%d,",vetorFinal[j]);
+
+                }else if(vetorFinal[j] != 0){
+                    System.out.printf("%d",vetorFinal[j]);
+                }
+            }
+        }else{
+            System.out.printf("Nenhum numero anterior digitado foi repetido!\n");
+        }
+        System.out.printf("}");
+    
+
     }
 }
--- Quinto_Exercicio.java
+++ Quinto_Exercicio.java
@@ -6,15 +6,15 @@ public class Quinto_Exercicio{
         public static void main(String[] args) {
             
             Scanner sc = new Scanner(System.in);
-            double[] SomaIdade = new double[2]; 
+            double[] SomaIdade = new double[5]; 
             double SomaAltura = 0;
             double contIdade = 0, contPeso = 0;
             int idade;
             double peso;
             double altura;
 
-            for (int i = 0; i < 2; i++) {
-                for (int j = 0; j < 3; j++) {
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 11; j++) {
                       
                     System.out.printf("%s - time / %s - jogador \n Digite a idade: ", i+1, j+1);
                     idade = sc.nextInt();
@@ -35,7 +35,7 @@ public class Quinto_Exercicio{
             }
 
             System.out.printf("Quatidade de jogadores com Idade Inferior a 18: %.0f\n", contIdade);
-            for (int i = 0; i < 2; i++) {
+            for (int i = 0; i < 5; i++) {
             System.out.printf("Media de Idade do %d - Time: %.2f\n", i+1, (SomaIdade[i] / 3.0));
             }
             System.out.printf("Media de Altura de todos os jogadores: %.2f\n", (SomaAltura / 6.0));
